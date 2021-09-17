fe4287b841d189d04ada2755dafd4611a0f117b0
Thorsten Sick <thorsten.sick@avast.com>
2021-09-17 11:38:56 +0200

    Init now installs latex dependencies. Auto generation of human readable documentation is now possible

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 # Init the system
 
 sudo apt-get -y install python3-venv
+sudo apt-get -y install latexmk texlive-fonts-recommended texlive-latex-recommended texlive-latex-extra
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
