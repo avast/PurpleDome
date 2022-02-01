0b9c2b7a8d2437b302e5a44d6a61d6174b5ee995
Thorsten Sick <thorsten.sick@avast.com>
2022-02-01 09:59:29 +0100

    Added argcomplete to command line tools

--- doc_generator.py
+++ doc_generator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+# PYTHON_ARGCOMPLETE_OK
 """ Generate human readable document describing the attack based on an attack log """
 
 import argparse
+import argcomplete
 from app.doc_generator import DocGenerator
 
 DEFAULT_ATTACK_LOG = "removeme/loot/2021_09_08___07_41_35/attack.json"  # FIN 7 first run on environment
@@ -10,16 +11,18 @@ DEFAULT_ATTACK_LOG = "removeme/loot/2021_09_08___07_41_35/attack.json"  # FIN 7 
 
 def create_parser():
     """ Creates the parser for the command line arguments"""
-    parser = argparse.ArgumentParser("Controls an experiment on the configured systems")
+    lparser = argparse.ArgumentParser("Controls an experiment on the configured systems")
 
-    parser.add_argument("--attack_log", default=DEFAULT_ATTACK_LOG, help="The attack log the document is based on")
-    parser.add_argument("--outfile", default="tools/human_readable_documentation/source/contents.rst", help="The default output file")
+    lparser.add_argument("--attack_log", default=DEFAULT_ATTACK_LOG, help="The attack log the document is based on")
+    lparser.add_argument("--outfile", default="tools/human_readable_documentation/source/contents.rst", help="The default output file")
 
-    return parser
+    return lparser
 
 
 if __name__ == "__main__":
-    arguments = create_parser().parse_args()
+    parser = create_parser()
+    argcomplete.autocomplete(parser)
+    arguments = parser.parse_args()
 
     dg = DocGenerator()
     dg.generate(arguments.attack_log, arguments.outfile)
