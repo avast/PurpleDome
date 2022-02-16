a69dc4cb4b008cc6923ceb023d2dc64c0b0be436
Thorsten Sick <thorsten.sick@avast.com>
2022-02-16 08:46:28 +0100

    PEP8 fix

--- presentations/intro_de/conf.py
+++ presentations/intro_de/conf.py
@@ -44,10 +44,6 @@ extensions += [
 # -- GraphViz configuration ----------------------------------
 graphviz_output_format = 'svg'
 
-
-
-
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
